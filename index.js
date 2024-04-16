64e506d657219b9181d696607e8d52991049899a
pirince <pliyo@ivytech.edu>
2024-04-16 14:05:17 -0500

    first commit

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ app.set("view engine", "ejs");
 app.listen(3000);
 app.get("/", (req, res) => {
   //res.send('<h1>Hello World!</h1>');
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render('index');
 });
 app.get("/about", (req, res) => {
   //res.send('<h1>Hello World! about page</h1>');
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render('about');
 });
 //redirect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 })
 //404 page
 app.use("*", (req, res) =>{
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render('404')
 })
