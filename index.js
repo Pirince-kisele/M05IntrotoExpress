45e5fb764d35840a1b350bc96b2217b04eba32e7
Pirince-kisele <156867185+Pirince-kisele@users.noreply.github.com>
2024-04-16 23:06:15 +0000

    dynamic data

--- index.js
+++ index.js
@@ -11,18 +11,23 @@ app.set("view engine", "ejs");
 // listens for request
 app.listen(3000);
 app.get("/", (req, res) => {
-  //res.send('<h1>Hello World!</h1>');
-  res.render('index');
+  //send the blog to the client;
+  const blogs =[
+    {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur"},
+     {title: "Mario finds stars", snippet: "Lorem ipsum dolor sit amet consectetur"},
+   {title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur"}
+  ]
+  res.render('index', {title: 'Home', blogs});
 });
 app.get("/about", (req, res) => {
   //res.send('<h1>Hello World! about page</h1>');
-  res.render('about');
+  res.render('about', {title: 'About'});
 });
 //redirect
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", {title: 'Create a blog'});
 })
 //404 page
 app.use("*", (req, res) =>{
-  res.status(404).render('404')
+  res.status(404).render('404' ,{title: '404'})
 })
