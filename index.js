ae341671086f7f9b31a6113710a60c40441da721
Pirince-kisele <156867185+Pirince-kisele@users.noreply.github.com>
2024-04-25 19:19:50 +0000

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 
 
 // connect to mongodb
-const dbURI = "mongodb+srv://pirinceBlogs:Miradi32Tembo@blogs.x7sgeik.mongodb.net/note-tuts?retryWrites=true&w=majority&appName=blogs"
+const dbURI = "mongodb+srv://pirinceBlogs:Miradi32Tembo@blogs.x7sgeik.mongodb.net/node-test?retryWrites=true&w=majority&appName=blogs";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then((result) => app.listen(port,()=> {console.log(`server is listening on port ${port}`)}), console.log("connected to mongodb and satrt the server"))
 .catch((err) => console.log(err));
@@ -22,6 +22,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 //middleware & static files in order to read static file like css images and some javascript
 app.use(morgan('dev'))
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true})) // this middlware is used to read the body of the request
 
 
 
@@ -33,17 +34,51 @@ app.get("/about", (req, res) => {
   //res.send('<h1>Hello World! about page</h1>');
   res.render('about', {title: 'About'});
 });
+//this is the form to create a new blog
+app.get("/blogs/create", (req, res) => {
+  res.render("create", {title: 'Create a blog'});
+})
 
 
 //Blogs Routes
+
+// this is the route to get all the blogs
 app.get("/blogs", (req, res) =>{
   Blog.find().sort({createdAt: -1})
   .then((result) => res.render('index', {blogs: result, title: 'All Blogs'}))
   .catch((err) => console.log(err));
 })
-app.get("/blogs/create", (req, res) => {
-  res.render("create", {title: 'Create a blog'});
+
+// create new blog in the database and save it in the database
+app.post( "/blogs",(req, res) =>{
+const blog = new Blog(req.body);// this is the object that we are going to save in the database
+  blog.save()
+  .then((result) => {
+    res.redirect('/blogs')
+  })
+  .catch((err) => {console.log(err)});
+  
 })
+// show indivisual blog detailes by fining the id of the blog
+app.get("/blogs/:id", (req, res) =>{
+const id = req.params.id;
+  Blog.findById(id)
+  .then((result) => {
+    res.render('details', {blog: result, title: 'Blog Details'})})
+  .catch((err) => {console.log(err)});
+})
+
+// delet blog by id
+app.delete("/blogs/:id", (req, res) =>{
+const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+  .then((result) => {
+    res.json({redirect: '/blogs'})
+  })
+     .catch((err) => {console.log(err)});
+})
+
+
 //404 page
 app.use("*", (req, res) =>{
   res.status(404).render('404' ,{title: '404'})
