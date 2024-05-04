aa1ba158a1329c6b87db63cb74cd4728c3e010c9
Pirince-kisele <156867185+Pirince-kisele@users.noreply.github.com>
2024-05-04 22:04:15 +0000

    finished

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -8,13 +8,13 @@ const router = express.Router();
 // this is the route to get all the blogs
 router.get("/blogs", blogController.blog_index);
 //this is the form to create a new blog
-router.get("blogs/create", blogController.blog_create_get);
+router.get("/create", blogController.blog_create_get);
 // create new blog in the database and save it in the database
 router.post("/blogs", blogController.blog_create_post);  
 // show indivisual blog detailes by fining the id of the blog
-router.get('/blogs:id', blogController.blog_details);
+router.get('/blogs/:id', blogController.blog_details);
 
 // delet blog by id
-router.delete("/blogs:id", blogController.blog_delete);
+router.delete("/blogs/:id", blogController.blog_delete);
 
 module.exports = router;
