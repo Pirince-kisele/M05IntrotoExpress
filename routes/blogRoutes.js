cdc9e2054939e2c73b1ce91bd3e9889d52afc33e
Pirince-kisele <156867185+Pirince-kisele@users.noreply.github.com>
2024-05-04 01:17:46 +0000

    update

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const router = express.Router();
-
+const express = require("express")
 // import the controller for blog`
 const blogController = require("../controllers/blogController");
+const router = express.Router();
 
 // All Blogs Routes
 
 // this is the route to get all the blogs
 router.get("/", blogController.blog_index);
 //this is the form to create a new blog
-router.get("/create", blogController.blog_create_post);
+router.post("/", blogController.blog_create_post);
 
 // create new blog in the database and save it in the database
-router.post("/", blogController.blog_create_get);
+router.get("/create", blogController.blog_create_get);
 // show indivisual blog detailes by fining the id of the blog
 router.get("/:id", blogController.blog_details);
 
